7cec47e19837cbc635591a1c47dd5af615b2928c
Trung Nguyễn <62378535+trungnt1000vn@users.noreply.github.com>
2023-03-21 11:29:29 +0700

    Complete 14:57 of Video6

--- Telesapp/Controllers/ProfileViewController.swift
+++ Telesapp/Controllers/ProfileViewController.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+import UIKit
+import FirebaseAuth
+class ProfileViewController : UIViewController{
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    let data = ["Log Out"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+}
+extension ProfileViewController : UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        cell.textLabel?.textAlignment = .center
+        cell.textLabel?.textColor = .red
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let actionSheet = UIAlertController(title: "", message: "", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Log Out", style: .destructive,handler: {[weak self] _ in
+            guard let strongSelf = self else{
+                return
+            }
+            do {
+                try FirebaseAuth.Auth.auth().signOut()
+                
+                let vc = LoginViewController()
+                let nav = UINavigationController(rootViewController: vc)
+                nav.modalPresentationStyle = .fullScreen
+                strongSelf.present(nav,animated: false)
+            }
+            catch {
+                print("Failed to log out")
+            }
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel,handler: nil))
+        present(actionSheet,animated: true)
+        
+    }
+}
