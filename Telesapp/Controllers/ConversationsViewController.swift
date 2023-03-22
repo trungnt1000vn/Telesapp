ada52ba6d7044a32b0b3569efb517c50cb93f263
Trung Nguyễn <62378535+trungnt1000vn@users.noreply.github.com>
2023-03-22 17:41:21 +0700

    Complete until 12:22

--- Telesapp/Controllers/ConversationsViewController.swift
+++ Telesapp/Controllers/ConversationsViewController.swift
@@ -7,10 +7,35 @@
 
 import UIKit
 import FirebaseAuth
+import JGProgressHUD
+
+
 class ConversationsViewController: UIViewController {
+    
+    private let spinner = JGProgressHUD(style: .dark)
+    
+    private let tableView: UITableView = {
+       let table = UITableView()
+        table.isHidden = true
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private let noConversationsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "No Conversations !"
+        label.textAlignment = .center
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 21, weight: .medium)
+        label.isHidden = true
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(tableView)
+        setupTableView()
+        fetchConversations()
     }
     override func viewDidAppear(_ animated: Bool) {
         
@@ -25,4 +50,24 @@ class ConversationsViewController: UIViewController {
             present(nav,animated: false)
         }
     }
+    private func setupTableView(){
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    private func fetchConversations(){
+        
+    }
+}
+extension ConversationsViewController:UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello World"
+        return cell
+    }
+    
+    
 }
