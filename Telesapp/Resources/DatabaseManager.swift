0e9915caac54469aeced6e7bddcd56b805e19184
Nguyễn Thành Trung <62378535+trungnt1000vn@users.noreply.github.com>
2023-03-25 18:22:32 +0700

    Complete video10

--- Telesapp/Resources/DatabaseManager.swift
+++ Telesapp/Resources/DatabaseManager.swift
@@ -46,11 +46,69 @@ extension DatabaseManager{
                 completion(false)
                 return
             }
+            /*
+             users => [
+                [
+                    "name":
+                    "safe_email":
+                ]
+             
+             ]
+             */
+            self.database.child("users").observeSingleEvent(of: .value, with: {
+                snapshot in
+                    if var usersCollection = snapshot.value as? [[String:String]]{
+                        //append to user dictionary
+                        let newElement: [[String:String]] = [
+                            [
+                             "name": user.firstName + " " + user.lastName,
+                             "email": user.safeEmail
+                            ]
+                        ]
+                        usersCollection.append(contentsOf: newElement)
+                        self.database.child("users").setValue(usersCollection,withCompletionBlock: { error , _ in
+                            guard error == nil else {
+                                completion(false)
+                                return
+                            }
+                            completion(true)
+                        })
+                    }
+                    else{
+                        //create that array
+                        let newCollection: [[String:String]] = [
+                            ["name": user.firstName + " " + user.lastName,
+                             "email": user.safeEmail
+                            ]
+                        ]
+                        self.database.child("users").setValue(newCollection,withCompletionBlock: { error , _ in
+                            guard error == nil else {
+                                completion(false)
+                                return
+                            }
+                            completion(true)
+                        })
+                    }
+            })
+            
             completion(true)
         })
     }
-    
+    public func getAllUsers(completion: @escaping (Result<[[String:String]], Error>) -> Void){
+        database.child("users").observeSingleEvent(of: .value, with: {
+            snapshot in
+            guard let value = snapshot.value as? [[String:String]] else{
+                completion(.failure(DatabaseError.failedToFetch))
+                return
+            }
+            completion(.success(value))
+        })
+    }
+    public enum DatabaseError: Error{
+        case failedToFetch
+    }
 }
+
 struct ChatAppUser{
     let firstName: String
     let lastName: String
