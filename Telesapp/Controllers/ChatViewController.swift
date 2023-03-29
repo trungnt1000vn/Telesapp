1cfb0551475f8bf7cf12bf49c90388eb7ea3bcf2
Trung Nguyễn <62378535+trungnt1000vn@users.noreply.github.com>
2023-03-29 16:37:13 +0700

    commit for check

--- Telesapp/Controllers/ChatViewController.swift
+++ Telesapp/Controllers/ChatViewController.swift
@@ -56,6 +56,9 @@ class ChatViewController: MessagesViewController{
         formatter.locale = .current
         return formatter
     }()
+    
+    private let conversationId: String?
+    
     public var isNewConversation = false
     
     public let otherUserEmail: String
@@ -67,13 +70,40 @@ class ChatViewController: MessagesViewController{
         guard let email = UserDefaults.standard.value(forKey: "email") as? String else{
             return nil
         }
+        let safeEmail = DatabaseManager.safeEmail(emailAddress: email)
       return Sender(photoURL: "",
-             senderId: email,
-             displayName: "Trung Đẹp Trai")
+             senderId: safeEmail,
+             displayName: "Me")
     }
-    init(with email: String){
+    private func listenForMessages(id: String){
+        DatabaseManager.shared.getAllMessagesForConversation(with: id, completion: {
+            [weak self] result in
+            switch result {
+            case .success(let messages):
+                guard !messages.isEmpty else {
+                    return
+                }
+                self?.messages = messages
+                
+                
+                DispatchQueue.main.async {
+                    self?.messagesCollectionView.reloadDataAndKeepOffset()
+                }
+                
+                
+            case .failure(let error):
+                print("Failed to get messages: \(error)")
+            }
+        })
+    }
+    
+    init(with email: String,id : String?){
         self.otherUserEmail = email
+        self.conversationId = id
         super.init(nibName: nil, bundle: nil)
+        if let conversationId = conversationId{
+            listenForMessages(id: conversationId)
+        }
     }
     
     required init?(coder: NSCoder) {
@@ -87,7 +117,6 @@ class ChatViewController: MessagesViewController{
         messagesCollectionView.messagesLayoutDelegate = self
         messagesCollectionView.messagesDisplayDelegate = self
         messageInputBar.delegate = self
-        
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -110,7 +139,7 @@ extension ChatViewController: InputBarAccessoryViewDelegate{
                                   messageId: messageId ,
                                   sentDate: Date(),
                                   kind: .text(text))
-            DatabaseManager.shared.createNewConversation(with: otherUserEmail, firstMessage: message, completion: { [weak self] success in
+            DatabaseManager.shared.createNewConversation(with: otherUserEmail,name: self.title ?? "User", firstMessage: message, completion: { [weak self] success in
                 if success{
                     print ("Message sent")
                 }
@@ -144,8 +173,8 @@ extension ChatViewController: MessagesDataSource, MessagesLayoutDelegate, Messag
         if let sender = selfSender{
             return sender
         }
+        
         fatalError(" Self Sender is nil, email should be cached")
-        return Sender(photoURL: "", senderId: "12", displayName: "")
     }
     
     func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessageKit.MessagesCollectionView) -> MessageKit.MessageType {
