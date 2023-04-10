4189f3135ac42bd46d054c0eddaf60864bd26ddf
Trung Nguyễn <62378535+trungnt1000vn@users.noreply.github.com>
2023-04-10 17:34:53 +0700

    Complete video 16 but still has bug

--- Telesapp/Controllers/ConversationsViewController.swift
+++ Telesapp/Controllers/ConversationsViewController.swift
@@ -143,4 +143,24 @@ extension ConversationsViewController:UITableViewDelegate,UITableViewDataSource{
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 120
     }
+    
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+           //begin delete
+            let conversationId = conversations[indexPath.row].id
+            tableView.beginUpdates()
+            DatabaseManager.shared.deleteConversation(conversationId: conversationId, completion: { [weak self]success in
+                if success {
+                    self?.conversations.remove(at: indexPath.row)
+                    tableView.deleteRows(at: [indexPath], with: .left)
+                }
+                
+            })
+            tableView.endUpdates()
+        }
+    }
 }
